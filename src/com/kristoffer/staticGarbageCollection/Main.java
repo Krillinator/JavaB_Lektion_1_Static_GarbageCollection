ca2e20d2dff0c8da7def260a36420e5625703a5a
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-10-11 09:46:05 +0200

    Example

--- src/com/kristoffer/staticGarbageCollection/Main.java
+++ src/com/kristoffer/staticGarbageCollection/Main.java
@@ -1,12 +1,31 @@
 package com.kristoffer.staticGarbageCollection;
 
 public class Main {
+
+    /* DEPRECATED
+        protected void finalize() {
+            System.out.println("Test Garbage Collection");
+        }
+    */
+
     public static void main(String[] args) {
 
         // Counter counterA = new Counter();
         // Counter counterB = new Counter();
+        Counter counterC = new Counter();
+        Counter counterD = new Counter();
 
-        Test.test();
+        // Garbage Collection - Mark for unreferenced Object (heap)
+        new Counter(); // Anonymous Object
+        counterC = null; // Nulling Reference
+        counterC = counterD; // Assigning Reference (to another)
+
+        System.out.println(counterC);   // Same Memory Address
+        System.out.println(counterD);   // Same Memory Address
+
+        System.gc();
+
+
 
 
         for (int i = 0; i < 5; i++) {
